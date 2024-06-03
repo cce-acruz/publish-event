5fe1da85b4eb582bf7043fe0c7be47c13dd7f8d9
cce_acruz <cce_acruz@uolinc.com>
2024-06-03 14:50:32 -0300

    Nova integração

---
+++ src/main/kotlin/br/com/worktools/publishevent/enums/emission/card/TrailStatusEnum.kt
@@ -0,0 +1,13 @@
+package br.com.worktools.publishevent.enums.emission.card
+
+enum class TrailStatusEnum {
+    ACTIVE,
+    INACTIVE,
+    CANCELLED,
+    BLOCKED,
+    BLOCKED_BY_ACCOUNT,
+    BLOCKED_BY_EMISSION,
+    RECHARGE_BLOCKED,
+    TEMPORARILY_BLOCKED,
+    UNKNOWN,
+}
